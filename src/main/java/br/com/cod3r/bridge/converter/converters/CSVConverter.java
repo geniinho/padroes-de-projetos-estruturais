0aabb70dac1c74f1d19474aa14ae157900b3f488
geniinho <geniinho@hotmail.com>
2022-05-05 17:31:30 -0300

    Padrão bridge

---
+++ src/main/java/br/com/cod3r/bridge/converter/converters/CSVConverter.java
@@ -0,0 +1,16 @@
+package br.com.cod3r.bridge.converter.converters;
+
+import br.com.cod3r.bridge.converter.employees.Employee;
+
+import java.util.Locale;
+
+public class CSVConverter implements Converter {
+
+	@Override
+	public String getEmployeeFormated(Employee emp) {
+		return String.format(Locale.US, "%s,%d,%.2f",
+				emp.getName(),
+				emp.getAge(),
+				emp.getSalary());
+	}
+}
